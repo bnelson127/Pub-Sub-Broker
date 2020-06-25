58cc441d20576113e6d3be9abc276da42b6b3e7a
bnelson127 <bnelson844@mail.snu.edu>
2020-06-25 15:08:00 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Paycom_Seminar_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
